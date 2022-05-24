eab8242456bbb724ff5c39c5b5afa788850ec10e
Nitesh Agarwala <niteshagarwal003@gmail.com>
2022-05-24 09:31:22 +0530

    Test Case

--- src/main/java/com/springboot/greencommute/service/UserServiceImpl.java
+++ src/main/java/com/springboot/greencommute/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.springboot.greencommute.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
--- target/generated-sources/annotations/com/springboot/greencommute/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/springboot/greencommute/mapper/UserMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-19T13:48:25+0530",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.15 (Private Build)"
+    date = "2022-05-24T09:16:09+0530",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.3 (Private Build)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
